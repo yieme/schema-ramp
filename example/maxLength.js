83be0d7a9799454f4bed166c5c7ef771f4aa5910
yieme <yieme@cormea.com>
2014-08-08 11:38:45 -0600

    add MaxLength example

---
+++ example/maxLength.js
@@ -0,0 +1,15 @@
+var obj2schema = require('../obj2schema')
+var obj        = require('./data.json')
+var options    = {
+  url: 'http://site.com',
+  ids: true,
+  absoluteIds: true,
+  default: true,
+  required: true,
+  verbose: true,
+  maxLength: 99
+}
+var schema     = obj2schema(obj, options)
+
+var pd         = require('pretty-data').pd
+console.log(pd.json(schema))
