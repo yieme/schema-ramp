8fb706fe24fffa0cfb63be615fa0bf5a38121839
yieme <yieme@cormea.com>
2014-08-08 11:41:29 -0600

    add auto.js example

---
+++ example/auto.js
@@ -0,0 +1,17 @@
+var obj2schema = require('../obj2schema')
+var obj        = require('./data.json')
+var options    = {
+  url: 'http://site.com',
+  ids: true,
+  absoluteIds: true,
+  default: true,
+  required: true,
+  verbose: true,
+  maxLength: 'auto',
+  maximum: 'auto',
+  verbose: 'auto'
+}
+var schema     = obj2schema(obj, options)
+
+var pd         = require('pretty-data').pd
+console.log(pd.json(schema))
